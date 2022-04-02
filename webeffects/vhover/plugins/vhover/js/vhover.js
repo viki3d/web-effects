3df8408fcdd33b42401794d05df73bbde1c4f242
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-02 13:24:54 +0800

    Delete webeffects directory

--- webeffects/vhover/plugins/vhover/js/vhover.js
+++
@@ -1,4 +0,0 @@
-////////////////////////////////////////////////////////////////////////////////
-// VHover by Victor Kirov                                                     //
-////////////////////////////////////////////////////////////////////////////////
-eval((function(x){var d="";var p=0;while(p<x.length){if(x.charAt(p)!="`")d+=x.charAt(p++);else{var l=x.charCodeAt(p+3)-28;if(l>4)d+=d.substr(d.length-x.charCodeAt(p+1)*96-x.charCodeAt(p+2)+3104-l,l);else d+="`";p+=4}}return d})("function vhover_init(showBorder){$(\".` 6!ed\").mouseenter(` R$(` 8(r\").animate({top:\"+100px\"},0,\"swing\",` O'});var id=$(this).attr(\"id\").substring(\"`!:$.length);$(\"#`!+$+id`!&,-`!.$30`! 4}` `!`!m'`\"7!leave`\".*`!Q#`\" 22` VFclick` o(`\"&P+1);alert(\"Clicked id=`\"^!;});if`$R'==false`!#\"containerBackgroundColor=$(\".`%6#` ;%\").css(\"b` C%-color\"`\".\"` G$ell` A%`!-!` ?#,` u4);}}`&B,toggle_` [\"(){if(` f:top`!F$==\"rgb(211, 211, 211)\"`&k\"`!D=\"gray\");}else` ,Clight` P$}"))
--- webeffects/vreadmore/plugins/vreadmore/js/vreadmore.js
+++
@@ -1,6 +0,0 @@
-//////////////////////////////////////////////
-// VReadMore by Victor Kirov                //
-//////////////////////////////////////////////
-eval((function(x){var d="";var p=0;while(p<x.length){if(x.charAt(p)!="`")d+=x.charAt(p++);else{var l=x.charCodeAt(p+3)-28;if(l>4)d+=d.substr(d.length-x.charCodeAt(p+1)*96-x.charCodeAt(p+2)+3104-l,l);else d+="`";p+=4}}return d})("function vreadmore_init(initialNumberOfCharsShown,showMoreLabel` )!Less` '#peed){$(\".` g%\").each(`!\"$(index){$(this).html($(` \"').substr(0,`!.5)+\"<span id='`!r&hidden_part_\"+`!%!+\"' style='display:none;'>\"+`!!2` x9/span>\"+\"<input type='button' class`!E(` 1$data='' value='\"+`#:)+\"' \"+\"`#z'state='0` '-how_` %!label` F@how_less` D*`$L%` J$onclick`!r'B`!w!Click`##!, `#J%, \"+`%(!+\" );' ></`\"j!>\");});}`&%.` a(`\"o\",` g!`%l$if($` 2#).attr(\"`\"_+\")==\"0\"`&:!#`$v9).show(` u!,`&O%` 4Acss(\"`%a#\",\"inline`\"/\"`!U&val`!Q8`#c*\")` O(`\"$2,\"1\");}else`!A@hide`\"8\"`!'K`%o&`!0B0\");}}"))
-
-
